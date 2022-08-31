c92d78280ea31dcac1375b97597b266329d825f8
Artur Moises Alves de Paula <arturmoiscontato@gmail.com>
2022-08-30 23:44:27 -0300

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db");
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.json(db.findUsers());
+});
+
+router.post('/', (req, res) => {
+  const user = db.insertUser(req.body);
+  res.status(201).json(user);
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const user = db.updateUser(id, req.body);
+  res.status(200).json(user);
+
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const user = db.deleteUser(id);
+  res.status(200).json(user);
+})
+
+module.exports = router;
